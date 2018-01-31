d69c586297e6731e3f924ab8255337fd1ddbd461
DenverBYF <851166182@qq.com>
2018-01-31 08:24:42 +0000

    修改路由配置

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,36 @@
 |
 */
 
-$router->get('/', ['middleware' => 'auth:users', function () use ($router) {
-    //echo "Code start";
-}]);
+/*
+ * 注册路由组
+ * */
 $router->group(['as' => 'register', 'prefix' => 'register'], function () use ($router) {
 	$router->post('user', ['uses' => 'users\registerController@index']);
 	$router->post('doctor', ['uses' => 'doctors\registerController@index']);
 });
+
+/*
+ * 登录路由组
+ * */
 $router->group(['as' => 'login', 'prefix' => 'login'], function () use ($router) {
 	$router->get('user', ['uses' => 'users\loginController@index']);
 	$router->post('doctor', ['uses' => 'doctors\loginController@index']);
 	$router->post('manager', ['uses' => 'manager\loginController@index']);
+});
+
+/*
+ * Api路由组
+ * 使用auth中间件进行身份认证
+ * */
+$router->group(['as' => 'api', 'prefix' => 'api'], function () use ($router) {
+	$router->group(['prefix' => 'user', 'middleware' => 'auth:users'], function () use ($router) {
+
+	});
+	$router->group(['prefix' => 'manager', 'middleware' => 'auth:manager'], function () use ($router){
+
+	});
+	$router->group(['prefix' => 'doctor', 'middleware' => 'auth:doctors'], function () use ($router){
+
+	});
 });
 
