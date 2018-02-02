38766a51c31e3f3d6f1c0e3c16e15fd49458a097
DenverBYF <851166182@qq.com>
2018-02-02 10:17:12 +0800

    增加同意登录中间件

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -13,12 +13,19 @@ class UserMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $role)
+    public function handle($request, Closure $next, $role = null)
     {
 
     	$sessionId = $request->header('token');
 		session_id($sessionId);
 		session_start();
+		if (empty($role)) {
+			if (isset($_SESSION['id'])) {
+				return $next($request);
+			} else {
+				return response()->json(['code' => -1, 'msg' => 'no auth']);
+			}
+		}
 		if (isset( $_SESSION['id'] ) and $_SESSION['type'] === $role) {
 			return $next($request);
 		} else {
