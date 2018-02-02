c95fc8b5ef85eb1afa20c96707ad7382da659521
DenverBYF <851166182@qq.com>
2018-02-02 01:20:37 +0000

    审批申请

---
+++ app/Http/Controllers/AcceptController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denverb
+ * Date: 18/2/2
+ * Time: 上午8:55
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+
+class AcceptController extends Controller
+{
+	protected $doctor;
+
+	//接收成为医生
+	public function acceptDoctor($id)
+	{
+		$this->doctor = DB::table('doctors')->select('id', 'status_type')->where('id', $id)->first();
+		if ($this->judgeType(1)) {
+			if (DB::table('doctors')->where('id', $id)->update(['status' => 2])) {
+				return response()->json(['code' => 0, 'msg' => 'success']);
+			}
+		} else {
+			return response()->json(['code' => -2, 'msg' => '申请类型与审批类型不一致']);
+		}
+
+	}
+
+	//接受成为医师
+	public function acceptPhysicians($id)
+	{
+		$this->doctor = DB::table('doctors')->select('id', 'status_type')->where('id', $id)->first();
+		if ($this->judgeType(2)){
+			if (DB::table('doctors')->where('id', $id)->update(['status' => 2])) {
+				return response()->json(['code' => 0, 'msg' => 'success']);
+			}
+		} else {
+			return response()->json(['code' => -2, 'msg' => '申请类型与审批类型不一致']);
+		}
+
+	}
+
+	//判断申请类型
+	protected function judgeType($type)
+	{
+		return $this->doctor->status_type == $type;
+	}
+
+}
